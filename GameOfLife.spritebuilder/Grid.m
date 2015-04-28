519250431cd86f7500accd88bf13d2e15e08c396
mac <mac@macs-iMac.local>
2015-04-28 09:01:10 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -143,6 +143,7 @@ static const int GRID_COLUMNS = 10;
 
 - (void) updateCreatures
 {
+    int numAlive = 0;
     for (int i = 0; i < [_gridArray count]; i++)
     {
         // iterate through all the columns for a given row
@@ -156,6 +157,9 @@ static const int GRID_COLUMNS = 10;
             } else if (neighbors <= 1 || neighbors >= 4) {
                 currentCreature.isAlive = false;
             }
+            
+            if (currentCreature.isAlive)
+                numAlive++;
         }
     }
 }
